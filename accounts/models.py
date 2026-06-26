747a5a07e28ca8c2b88b215867da26fd2b5c9c8c
Parthiv <kothariparthiv26@gmail.com>
2026-06-27 01:35:29 +0530

    run python manage.py migrate in your pc

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
+class Profile(models.Model):
+    STATUS_VIEWER='viewer'
+    STATUS_CREATOR='creator'
+    STATUS_CHOICES=[(STATUS_VIEWER,'Viewer')
+                    ,
+                    (STATUS_CREATOR,'Creator')]
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='viewer')
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username} ({self.status})"
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
