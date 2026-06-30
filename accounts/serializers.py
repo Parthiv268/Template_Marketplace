cfcddb3ed729910e08b4832b8714e77742491725
Parthiv <kothariparthiv26@gmail.com>
2026-06-30 18:37:38 +0530

    Added me/ to the code

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-
+from .models import Profile
 class RegisterSerializer(serializers.ModelSerializer):
     password=serializers.CharField(write_only=True)
     # means password is accepted but will never go out
@@ -15,3 +15,10 @@ class RegisterSerializer(serializers.ModelSerializer):
                 password=validated_data['password'] 
                 )
         return user
+class ProfileSerializer(serializers.ModelSerializer):
+    username=serializers.CharField(source='user.username',read_only=True)
+    email=serializers.CharField(source='user.email',read_only=True)
+    class Meta:
+        model=Profile
+        fields=['username','email','status','bio','profile_picture']
+    
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView,MeView
 urlpatterns = [
     path('register/',RegisterView.as_view(),name='register'),
+    path('me/',MeView.as_view(),name='me')
 ]
 # .asas_view() convert the class into a function that can be called.
